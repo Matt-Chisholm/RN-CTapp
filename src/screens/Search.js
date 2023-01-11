bb5cb8dbc18495dafcb4b8b4da26505b84938bcf
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-01-11 10:54:04 -0800

    explore api results

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,10 +1,28 @@
-import { View, Text, StyleSheet, TextInput } from "react-native";
-import React, { useState } from "react";
-import api from "../api/searcher";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import React, { useState, useEffect } from "react";
+import { API_KEY } from "@env";
+import axios from "axios";
 
 export default function Search() {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
   console.log(term);
+
+  const options = {
+    method: "GET",
+    url: "https://the-cocktail-db.p.rapidapi.com/search.php",
+    params: { s: term },
+    headers: {
+      "X-RapidAPI-Key": API_KEY,
+      "X-RapidAPI-Host": "the-cocktail-db.p.rapidapi.com",
+    },
+  };
+
+  const search = async () => {
+    const response = await axios.request(options);
+    setResults(response.data.drinks[0]);
+    console.log(results);
+  };
 
   return (
     <View>
@@ -16,6 +34,7 @@ export default function Search() {
         value={term}
         onChangeText={(newTerm) => setTerm(newTerm)}
       />
+      <Button title='Search' onPress={search} />
     </View>
   );
 }
