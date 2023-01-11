af96960d3c4bdcf688ffeb5dd4555463f3639caf
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-01-11 10:45:02 -0800

    search term track

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,10 +1,21 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+import React, { useState } from "react";
+import api from "../api/searcher";
 
 export default function Search() {
+  const [term, setTerm] = useState("");
+  console.log(term);
+
   return (
     <View>
       <Text>Search</Text>
+      <TextInput
+        style={{ height: 40, width: 200, borderColor: "gray", borderWidth: 1 }}
+        autoCapitalize='none'
+        autoCorrect={false}
+        value={term}
+        onChangeText={(newTerm) => setTerm(newTerm)}
+      />
     </View>
   );
 }
