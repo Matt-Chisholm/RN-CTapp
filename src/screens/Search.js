712352ba7b2ccb34c7c9684d681b13527e1272aa
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-01-11 11:27:46 -0800

    new direction

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,51 +1,25 @@
 import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 import React, { useState, useEffect, useCallback } from "react";
-import { API_KEY } from "@env";
-import axios from "axios";
+import ChatBot from "react-native-chatbot";
 
 export default function Search() {
-  const [term, setTerm] = useState("");
-  const [search, setSearch] = useState(false);
-  const [results, setResults] = useState([]);
-
-  const options = {
-    method: "GET",
-    url: "https://the-cocktail-db.p.rapidapi.com/search.php",
-    params: { s: term },
-    headers: {
-      "X-RapidAPI-Key": API_KEY,
-      "X-RapidAPI-Host": "the-cocktail-db.p.rapidapi.com",
+  const steps = [
+    {
+      id: "0",
+      message: "Hello, Welcome to the bar!",
+      trigger: "1",
     },
-  };
-
-  const makeRequest = useCallback(async () => {
-    try {
-      const response = await axios.request(options);
-      console.log(response.data.drinks[0]);
-      setResults(response.data.drinks[0]);
-    } catch (error) {
-      console.log(error);
-    }
-  }, [options]);
-
-  useEffect(() => {
-    if (search) {
-      makeRequest();
-      setSearch(false);
-    }
-  }, [search, term, makeRequest]);
+    {
+      id: "1",
+      message: "What is your name?",
+      trigger: "name",
+    },
+  ];
 
   return (
     <View>
       <Text>Search</Text>
-      <TextInput
-        style={{ height: 40, width: 200, borderColor: "gray", borderWidth: 1 }}
-        autoCapitalize='none'
-        autoCorrect={false}
-        value={term}
-        onChangeText={(newTerm) => setTerm(newTerm)}
-      />
-      <Button title='Search' onPress={() => setSearch(true)} />
+      <ChatBot steps={steps} />
     </View>
   );
 }
