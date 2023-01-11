a3d1eb973f6eb1e66ece6bcee984c8c4dc8a5540
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-01-11 10:42:05 -0800

    api and env

---
+++ src/screens/Search.js
@@ -0,0 +1,12 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+export default function Search() {
+  return (
+    <View>
+      <Text>Search</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
